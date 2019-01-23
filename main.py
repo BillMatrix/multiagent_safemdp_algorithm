1e4aef5d76f32581e9273dc0f53561d1e29b44ec
billmatrix <zheqzhu@stanford.edu>
2019-01-22 23:34:22 -0800

    nit change

--- main.py
+++ main.py
@@ -312,11 +312,6 @@ def main(args):
         print('Collaborative Joint Unsafe States')
         print(collaborative_joint_unsafe)
 
-    for i in range(world_shape[0]):
-        for j in range(world_shape[1]):
-            
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('map', help='enter either random or mars')
